856133698bafa1c0febcb7bcdfc79ebe17f13761
kunholee <gunho1020@gmail.com>
2018-07-15 21:12:18 +0900

    1: Header part
    - install and apply 'styled-components'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+  box-shadow: 0 10px 8px #EEEEEE;
+  font-size: 3rem;
+  background-color: #B3E5FC;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello React
+        <Header>
+          Simple Memo App
+        </Header>
       </div>
     );
   }
