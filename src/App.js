46af516fe3ccd04b80100e012767657fb297afe8
kunholee <gunho1020@gmail.com>
2018-07-18 21:32:03 +0900

    9, 11: Separate functional component, Reduce duplicate code
    - Make presenter.js
    - Put all JSX codes to presenter.js
    - Separate function for finding memo object in memo list
    - Separate function for finding memo index in memo list
    - Separate function for check if object's id is same with the given id
    - Make function for return negative value of the given value

--- src/App.js
+++ src/App.js
@@ -1,70 +1,14 @@
 import React, { Component } from 'react';
-import styled, { css } from 'styled-components';
 import uuidv1 from 'uuid/v1';
+import Presenter from './presenter';
 
-const Header = styled.header`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 4rem;
-  box-shadow: 0 10px 8px #eeeeee;
-  font-size: 3rem;
-  background-color: #b3e5fc;
-`;
+const find = (predi, list) => list.find(predi);
 
-const InputField = styled.div`
-  display: flex;
-  margin: 1rem 30rem;
-  height: 2rem;
-`;
+const findIndex = (predi, list) => list.findIndex(predi);
 
-const InputNew = styled.input`
-  flex-grow: 1;
-  font-size: 1.2rem;
-  border: 1px solid #0d47a1;
-  border-radius: 0.2rem;
-  padding-left: 0.4rem;
-`;
+const not = val => !val;
 
-const Button = styled.button`
-  ${props =>
-    props.width &&
-    css`
-      width: ${props.width};
-    `} margin-left: 1rem;
-  border: 0 none;
-  outline: 0 none;
-  background-color: #b2ebf2;
-  border-radius: 0.2rem;
-  &:hover {
-    background-color: #80deea;
-    cursor: pointer;
-    opacity: 0.8;
-  }
-  &:active {
-    opacity: 1;
-  }
-`;
-
-const Card = styled.div`
-  display: flex;
-  height: 2rem;
-  margin: 2rem 30rem 0;
-`;
-
-const InputEdit = styled.input`
-  flex-grow: 1;
-  font-size: 1.2rem;
-  border: 1px solid #0d47a1;
-  border-radius: 0.2rem;
-  padding-left: 0.4rem;
-  background-color: #e0f7fa;
-`;
-
-const CardList = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+const isSameId = id => obj => obj.id === id;
 
 const createNewMemo = id => prevState => ({
   newMemo: '',
@@ -80,12 +24,12 @@ const createNewMemo = id => prevState => ({
 });
 
 const deleteMemo = id => prevState => ({
-  memos: prevState.memos.filter(memo => memo.id !== id),
+  memos: prevState.memos.filter(memo => not(isSameId(id)(memo))),
 });
 
 const startEdit = id => prevState => {
-  const editingMemo = prevState.memos.find(memo => memo.id === id);
-  const idx = prevState.memos.findIndex(memo => memo.id === id);
+  const editingMemo = find(isSameId(id), prevState.memos);
+  const idx = findIndex(isSameId(id), prevState.memos);
   return {
     memos: [
       ...prevState.memos.slice(0, idx),
@@ -96,8 +40,8 @@ const startEdit = id => prevState => {
 };
 
 const inputEditing = (id, memo) => prevState => {
-  const editingMemo = prevState.memos.find(memo => memo.id === id);
-  const idx = prevState.memos.findIndex(memo => memo.id === id);
+  const editingMemo = find(isSameId(id), prevState.memos);
+  const idx = findIndex(isSameId(id), prevState.memos);
   return {
     memos: [
       ...prevState.memos.slice(0, idx),
@@ -108,8 +52,8 @@ const inputEditing = (id, memo) => prevState => {
 };
 
 const saveEditing = id => prevState => {
-  const editingMemo = prevState.memos.find(memo => memo.id === id);
-  const idx = prevState.memos.findIndex(memo => memo.id === id);
+  const editingMemo = find(isSameId(id), prevState.memos);
+  const idx = findIndex(isSameId(id), prevState.memos);
   return {
     memos: [
       ...prevState.memos.slice(0, idx),
@@ -125,8 +69,8 @@ const saveEditing = id => prevState => {
 };
 
 const cancelEditing = id => prevState => {
-  const editingMemo = prevState.memos.find(memo => memo.id === id);
-  const idx = prevState.memos.findIndex(memo => memo.id === id);
+  const editingMemo = find(isSameId(id), prevState.memos);
+  const idx = findIndex(isSameId(id), prevState.memos);
   return {
     memos: [
       ...prevState.memos.slice(0, idx),
@@ -168,48 +112,17 @@ class App extends Component {
     } = this;
     const { newMemo, memos } = this.state;
     return (
-      <div className="App">
-        <Header>Simple Memo App</Header>
-        <InputField>
-          <InputNew
-            placeholder="Input the memo"
-            value={newMemo}
-            onInput={_inputNewMemo}
-          />
-          <Button width="11rem" onClick={_createNewMemo}>
-            CREATE
-          </Button>
-        </InputField>
-        <CardList>
-          {memos.map(memo => (
-            <Card key={memo.id}>
-              <InputEdit
-                value={memo.isEditing ? memo.editingContent : memo.content}
-                disabled={!memo.isEditing}
-                onChange={event => _inputEditing(memo.id, event)}
-              />
-              {memo.isEditing ? (
-                <Button width="5rem" onClick={() => _saveEditing(memo.id)}>
-                  Save
-                </Button>
-              ) : (
-                <Button width="5rem" onClick={() => _startEdit(memo.id)}>
-                  Edit
-                </Button>
-              )}
-              {memo.isEditing ? (
-                <Button width="5rem" onClick={() => _cancelEditing(memo.id)}>
-                  Cancel
-                </Button>
-              ) : (
-                <Button width="5rem" onClick={() => _deleteMemo(memo.id)}>
-                  Delete
-                </Button>
-              )}
-            </Card>
-          ))}
-        </CardList>
-      </div>
+      <Presenter
+        newMemo={newMemo}
+        memos={memos}
+        inputNewMemo={_inputNewMemo}
+        inputEditing={_inputEditing}
+        createNewMemo={_createNewMemo}
+        saveEditing={_saveEditing}
+        startEdit={_startEdit}
+        cancelEditing={_cancelEditing}
+        deleteMemo={_deleteMemo}
+      />
     );
   }
 
